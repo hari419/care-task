2308c791bbf3fd5199451e19324b86a3c1dba8e1
Harishankar <harib@wavelabs.in>
2018-03-16 11:30:15 +0530

    footer-task

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // $("#header").load("header/header.html");
+    // $("#profile").load("profile/profile.html");
+    $("#footer").load("footer/footer.html");
+    
+});
